8213b56370bf848ed3f64cd7ac91ccbaab25e8c4
korostelv <valenok2025@gmail.com>
2025-02-04 16:41:50 +0500

    style

--- wiki/mysite/urls.py
+++ wiki/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import index, PersonageListView, ReleaseListView, ReleaseDetailView, GamerListView, GamerDetailView, ReleasAPIView
+from .views import  PersonageListView, ReleaseListView, ReleaseDetailView, GamerListView, GamerDetailView,PersonageDetailView, ReleasAPIView
 
 
 
@@ -12,11 +12,12 @@ router.register('api/releases', ReleasAPIView)
 
 
 urlpatterns = [
-    path('', index, name="index"),
+    # path('', index, name="index"),
     path("personages/", PersonageListView.as_view(), name="personages"),
-    path("releases/", ReleaseListView.as_view(), name="releases"),
+    path("", ReleaseListView.as_view(), name="releases"),
     path("gamers/", GamerListView.as_view(), name="gamers"),
     path('releases/<int:pk>/', ReleaseDetailView.as_view(), name='release_detail'),
     path('gamers/<int:pk>/', GamerDetailView.as_view(), name='gamer_detail'),
+    path('personages/<int:pk>/', PersonageDetailView.as_view(), name='personage_detail'),
 ]
 urlpatterns += router.urls
--- wiki/mysite/views.py
+++ wiki/mysite/views.py
@@ -7,25 +7,26 @@ from rest_framework import viewsets
 from .serializers import ReleaseSerializer
 from .models import Personage, Release, Gamer
 
-def index(request):
-    return render(request, "index.html")
+# def index(request):
+#     return render(request, "index.html")
 
 class PersonageListView(ListView):
     model = Personage
-    paginate_by = 10
+    paginate_by = 15
     allow_empty = False
     template_name = 'personages.html'
 
 
 class ReleaseListView(ListView):
     model = Release
-    paginate_by = 10
+    paginate_by = 12
     allow_empty = False
     template_name = 'releases.html'
 
 
 class GamerListView(ListView):
     model = Gamer
+    paginate_by = 15
     template_name = 'gamers.html'
 
 
@@ -37,6 +38,11 @@ class ReleaseDetailView(DetailView):
 class GamerDetailView(DetailView):
     model = Gamer
     template_name = 'gamer_detail.html'
+
+
+class PersonageDetailView(DetailView):
+    model = Personage
+    template_name = 'personage_detail.html'
 
 
 
