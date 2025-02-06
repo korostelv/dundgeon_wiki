0755c0fa8161edb51d9bc12186d989fbe41cc916
korostelv <valenok2025@gmail.com>
2025-02-06 13:01:04 +0500

    final style & template

--- wiki/mysite/views.py
+++ wiki/mysite/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
 
-
 from rest_framework import viewsets
-from .serializers import ReleaseSerializer
-from .models import Personage, Release, Gamer
+from .serializers import ReleaseSerializer, RaceSerializer, PersonageSerializer, GamerSerializer, StorylineSerializer
+from .models import Personage, Release, Gamer, Race, Storyline
 
-# def index(request):
-#     return render(request, "index.html")
 
 class PersonageListView(ListView):
     model = Personage
@@ -40,14 +36,62 @@ class GamerDetailView(DetailView):
     model = Gamer
     template_name = 'gamer_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        gamer_id = self.kwargs.get('pk')
+        context["releases"] = Release.objects.filter(gamers__pk=gamer_id).order_by('number')
+        return context
+
 
 class PersonageDetailView(DetailView):
     model = Personage
     template_name = 'personage_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        personage_id = self.kwargs.get('pk')
+        context["releases"] = Release.objects.filter(personages__pk=personage_id).order_by('number')
+        return context
 
 
+class PersonageListFilterView(ListView):
+    model = Personage
+    paginate_by = 15
+    allow_empty = False
+    template_name = 'personages_filter.html'
 
+    def get_queryset(self):
+        race_id = self.kwargs.get('pk')
+        return Personage.objects.filter(race=race_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        race_id = self.kwargs.get('pk')
+        context["race"] = Race.objects.get(pk=race_id).name
+        return context
+
+
+# API
 class ReleasAPIView(viewsets.ModelViewSet):
     queryset = Release.objects.all()
     serializer_class = ReleaseSerializer
+
+
+class RacesAPIView(viewsets.ModelViewSet):
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
+
+
+class PersonagesAPIView(viewsets.ModelViewSet):
+    queryset = Personage.objects.all()
+    serializer_class = PersonageSerializer
+
+
+class GamersAPIView(viewsets.ModelViewSet):
+    queryset = Gamer.objects.all()
+    serializer_class = GamerSerializer
+
+
+class StorylinesAPIView(viewsets.ModelViewSet):
+    queryset = Storyline.objects.all()
+    serializer_class = StorylineSerializer
