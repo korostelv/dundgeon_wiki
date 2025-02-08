45fb39b39111f1633a9279374f2c40f2a7f320d2
korostelv <valenok2025@gmail.com>
2025-02-08 15:03:57 +0500

    drf-auth

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -131,5 +131,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ]
+
+}
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
 }
