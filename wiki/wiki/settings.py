27dae229436d8a2064119128d36c194a6a4febf0
korostelv <valenok2025@gmail.com>
2025-02-12 12:17:11 +0500

    add migrate postgresql

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -64,23 +64,23 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'pay_db',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dundgeon',
+        'USER': 'valenok2025',
+        'PASSWORD': 'valenok2025',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
@@ -142,3 +142,4 @@ GRAPH_MODELS = {
   'all_applications': True,
   'group_models': True,
 }
+
